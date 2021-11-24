4e463fce23c19a32b13836b7858d1790c46bb06d
lorian10 <90500118+lorian10@users.noreply.github.com>
2021-11-24 13:41:40 +0100

    Reupload Drill 08

--- ch08/swap.cpp
+++ ch08/swap.cpp
@@ -24,49 +24,44 @@ int main(){
 
 	int x = 7;
 	int y = 9;
-	swap_v(x, y);
+	//swap_v(x, y);  Lefordítja de nem cserél
 	swap_r(x, y);
+	cout << "value of x: " << x << " value of y: " << y << endl;
 	//swap_cr(x, y);
-	swap_v(7, 9);
-	//swap_r(7, 9);
+	
+	//swap_v(7, 9);
+	//swap_r(x, 9); Nem fordítható le
 	//swap_cr(7, 9);
 	
-	cout << "value of x: " << x << ", value of y: " << y << endl;
+	
 	
 	const int cx = 7;
     	const int cy = 9;
     	
-    	swap_v(cx, cy);
-    	
+    	//swap_v(cx, cy);
     	//swap_r(cx, cy);
- 
-    	
     	//swap_cr(cx, cy);
-    	
-	swap_v(7.7, 9.9);   
-    	
+    
+	
+   
+	//swap_v(7.7, 9.9);   
 	//swap_r(7.7, 9.9);   
-       
 	//swap_cr(7.7, 9.9);
        
-	cout << "value of cx: " << cx << ", value of cy: " << cy << endl;
+	cout << "value of cx: " << cx << " value of cy: " << cy << endl;
 	
 	double dx = 7.7;
     	double dy = 9.9;
     	
-    	swap_v(cx, cy);
+    	//swap_v(dx, dy);
+    	//swap_r(dx, dy);
+    	//swap_cr(dx, dy);
     	
-    	//swap_r(cx, cy);
-    	
-    	//swap_cr(cx, cy);
-    	
-	swap_v(7.7, 9.9);   
-    	
+	//swap_v(7.7, 9.9);   
 	//swap_r(7.7, 9.9);   
-       
 	//swap_cr(7.7, 9.9);
     	
-    	cout << "value of dx: " << dx << ", value of dy: " << dy << endl;
+    	cout << "value of dx: " << dx << " value of dy: " << dy << endl;
 
 	return 0;
 
